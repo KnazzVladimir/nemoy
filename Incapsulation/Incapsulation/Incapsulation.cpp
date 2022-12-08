26aff342b9b5bf2e7680c6b8f127e4169db194c0
PoedatelTuristov <98821423+KnazzVladimir@users.noreply.github.com>
2022-12-08 22:42:43 +0500

    Add files via upload

---
+++ Incapsulation/Incapsulation/Incapsulation.cpp
@@ -0,0 +1,43 @@
+﻿// Incapsulation.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <tuple>
+using namespace std;
+class test1
+{
+private:
+    int* blankData;
+   public:
+    test1(int length)
+    {
+        this->blankData = new int[length];
+    }
+    void fillDynamicArray(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            blankData[i] =rand() % 19;
+        }
+    }
+    void showDynamicArray(int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            cout <<blankData[i] << " ";
+        }
+    }
+    ~test1()
+    {
+        delete[] blankData;
+    }
+};
+int main()
+{
+    setlocale(LC_ALL, ("rus"));
+    int num = 100;
+    test1 ad(num);
+    ad.fillDynamicArray(num);
+    ad.showDynamicArray(num);
+    
+}
